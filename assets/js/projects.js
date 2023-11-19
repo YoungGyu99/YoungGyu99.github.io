7b6fc1f861fdbb6da48bdfd876956e15cd82807b
YoungGyu99 <147577156+YoungGyu99@users.noreply.github.com>
2023-11-20 02:12:00 +0900

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,13 +11,11 @@ let render_projects = (slug) => {
 
     let projects_obj = [
         {
-            image: 'assets/images/mentors.jpg',
-            link: 'https://github.com/abhn/Mporter',
-            title: 'Mporter',
-            demo: 'https://mporter.co',
-            technologies: ['Flask', 'Celery', 'Python'],
-            description: "Flask web application for easy reporting updates to one's mentor. Multi-user support, easy to deploy and use.",
-            categories: ['featured', 'webdev']
+            image: 'assets/images/FestivalGuideProject.png',
+            link: 'https://github.com/YoungGyu99/FestivalGuideProject',
+            title: '축제길잡이',
+            technologies: ['Java', 'Oracle', 'Spring', 'Apache Tomcat],
+            description: "축제의 관객이 아닌 축제 관리자의 시점으로 축제 컨텐츠를 추천해주고 기획서 작성에 도움을 주기 위한 목적으로 제작된 축제길잡이",
         },
         {
             image: 'assets/images/mobile-landscape.jpg',
